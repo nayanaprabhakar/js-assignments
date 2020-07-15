18a8d26b9e91824b72aed3fcce462da50681f9cb
nayanaprabhakar <68281742+nayanaprabhakar@users.noreply.github.com>
2020-07-15 14:41:13 +0530

    Add files via upload

---
+++ day1q1.js
@@ -0,0 +1,12 @@
+console.log("Hey lu this is me");
+//is used to print the output to the console
+console.warn("This is a warning");
+//used to ,og warning message to the console
+console.timeEnd("Time taken");
+//whenever we want to know the amount of time spent by a block or a function,
+//we can make use of time() and timeEnd()
+
+var a={name:"abc",age:"12",place:"manali"}
+var b={name:"def",age:"13",place:"uttarpradesh"}
+var c={name:"ijk",age:"14",place:"switzerland"}
+console.table({a, b, c})
