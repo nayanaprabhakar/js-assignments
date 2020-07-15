cf4bb5e4097c7754be765592d6e7f5c36f952271
nayanaprabhakar <68281742+nayanaprabhakar@users.noreply.github.com>
2020-07-15 15:11:53 +0530

    Add files via upload

---
+++ day1q2.js
@@ -0,0 +1,21 @@
+//var variables can be uploaded andre-declared within its scope
+//let variables can be updated but not re-declared
+//const variables can neither be updated nor re-declared
+
+var name="xyz";
+var age="123";
+var canSing=true;
+var languages=["hindi","kannada","english"]
+
+
+var friends={
+    name:"abc",
+    hobby:"dancing",
+}
+
+var a=null;
+console.log(a);
+
+
+var b=10;
+console.log(b);
