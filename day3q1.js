c53be7598e157fd13b4a931f5328655e526a4633
nayanaprabhakar <68281742+nayanaprabhakar@users.noreply.github.com>
2020-07-18 16:42:46 +0530

    Day 3 assignment 1

---
+++ day3q1.js
@@ -0,0 +1,13 @@
+let num=prompt("enter the number");
+function oddeven(){
+    console.log('the number entered is ${num}');
+    if (num %2 ==0){
+        console.log("the number is even");
+    }else{
+        console.log("the number is odd")
+    }
+}
+oddeven();
+
+
+    
